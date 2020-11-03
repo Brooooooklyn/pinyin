f8330351b3e0fee68507651fb06211434b4820ac
github-action-benchmark <github@users.noreply.github.com>
2020-11-03 17:24:01 +0000

    add Benchmark (benchmarkjs) benchmark result for 9c70e16a1c18812ae6709774def6eaf6768ffdf5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1604241807666,
+  "lastUpdate": 1604424241863,
   "repoUrl": "https://github.com/Brooooooklyn/pinyin",
   "entries": {
     "Benchmark": [
@@ -78,6 +78,86 @@ window.BENCHMARK_DATA = {
             "name": "Long input with segment#node-pinyin",
             "value": 2,
             "range": "±4.25%",
+            "unit": "ops/sec",
+            "extra": "9 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lynweklm@gmail.com",
+            "name": "LongYinan",
+            "username": "Brooooooklyn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9c70e16a1c18812ae6709774def6eaf6768ffdf5",
+          "message": "Merge pull request #2 from Brooooooklyn/fix-mixed\n\nFix mixed Chinese and non Chinese words",
+          "timestamp": "2020-11-04T01:19:19+08:00",
+          "tree_id": "c89009ff7960d941238e66f0e3edc513533ecb9f",
+          "url": "https://github.com/Brooooooklyn/pinyin/commit/9c70e16a1c18812ae6709774def6eaf6768ffdf5"
+        },
+        "date": 1604424241190,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Short input without segment#@napi-rs/pinyin",
+            "value": 519263,
+            "range": "±0.44%",
+            "unit": "ops/sec",
+            "extra": "90 samples"
+          },
+          {
+            "name": "Short input without segment#node-pinyin",
+            "value": 281616,
+            "range": "±0.29%",
+            "unit": "ops/sec",
+            "extra": "90 samples"
+          },
+          {
+            "name": "Long input without segment#@napi-rs/pinyin",
+            "value": 37,
+            "range": "±2.01%",
+            "unit": "ops/sec",
+            "extra": "51 samples"
+          },
+          {
+            "name": "Long input without segment#node-pinyin",
+            "value": 1,
+            "range": "±3.12%",
+            "unit": "ops/sec",
+            "extra": "8 samples"
+          },
+          {
+            "name": "Short input with segment#@napi-rs/pinyin",
+            "value": 311301,
+            "range": "±2.88%",
+            "unit": "ops/sec",
+            "extra": "95 samples"
+          },
+          {
+            "name": "Short input with segment#node-pinyin",
+            "value": 201697,
+            "range": "±0.22%",
+            "unit": "ops/sec",
+            "extra": "92 samples"
+          },
+          {
+            "name": "Long input with segment#@napi-rs/pinyin",
+            "value": 87,
+            "range": "±0.36%",
+            "unit": "ops/sec",
+            "extra": "74 samples"
+          },
+          {
+            "name": "Long input with segment#node-pinyin",
+            "value": 2,
+            "range": "±2.33%",
             "unit": "ops/sec",
             "extra": "9 samples"
           }
